61f54fba3f071810e02c17bc79b417f2fd3a8780
filipeqm94 <queiroz.filipe94@gmail.com>
2021-10-29 16:47:33 -0400

    solve commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+//variables
+const setColorBtn = document.querySelector("#set-color");
+const color = document.querySelector("#color-field");
+const brush = document.querySelector(".brush");
 
+//functions
+function setColor(event) {
+  event.preventDefault();
+  brush.style.backgroundColor = color.value;
+  color.value = "";
+}
+
+//script
+
+setColorBtn.addEventListener("click", setColor);
